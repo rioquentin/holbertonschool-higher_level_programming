fcbd52a6bf2d9693135b2776940fb0b41bd06767
quentin <quentin.rio.pro@gmail.com>
2022-09-08 01:17:23 -0700

    fixed xtra -

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,8 @@
 def print_last_digit(number):
     if number < 0:
         number = number % -10
+        number = str(number)
+        number = int(number[-1:])
     else:
         number = number % 10
     print(number, end='')
