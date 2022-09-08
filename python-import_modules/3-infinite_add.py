161b957f564d0926f95d43d3439cdd6ef5fef64d
quentin <quentin.rio.pro@gmail.com>
2022-09-08 07:46:22 -0700

    infinite add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-res = 0
-for i in range(1, len(sys.argv)):
-    res = res + int(sys.argv[i])
-print(res)
+if __name__ == "__main__":
+    res = 0
+    for i in range(1, len(sys.argv)):
+        res = res + int(sys.argv[i])
+    print(res)
