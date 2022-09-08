2f963747e219c18bffdcde38f98cf3ff20db32a5
quentin <quentin.rio.pro@gmail.com>
2022-09-08 07:02:45 -0700

    fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    print('{} arguments:'.format(len(sys.argv) - 1))
+    if (len(sys.argv) - 1) == 0:
+        print("0 arguments.")
+    else:
+        print('{} {}:'.format(len(sys.argv) - 1, "argument" if len(sys.argv) - 1 == 1 else "arguments"))
     for i in range(1, len(sys.argv)):
         print('{}: {}'.format(i, sys.argv[i]))
