87af318d213bc567ea15ca249e9c3c59116b35cd
quentin <quentin.rio.pro@gmail.com>
2022-09-08 01:08:18 -0700

    test

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 islower = __import__('7-islower').islower
+def _print(c):
+    print(chr(ord(c) - 32), end='')
+
 def uppercase(str):
     for i in str:
         if islower(i) == 1:
-            print(chr(ord(i) - 32), end='')
+            _print(i)
         else:
             print('{}'.format(i), end='')
+    print("")
