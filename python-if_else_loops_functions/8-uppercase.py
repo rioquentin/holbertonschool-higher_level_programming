30cf5a4dfdc333f9d35f3382b0b0352b1801ba5e
quentin <quentin.rio.pro@gmail.com>
2022-09-08 01:56:41 -0700

    changed all and used condition in print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 islower = __import__('7-islower').islower
-_print = __import__('func')._print
 def uppercase(str):
     for i in str:
-        if islower(i) == 1:
-            _print(i)
-        else:
-            print('{}'.format(i), end='')
-    print("")
+        print('{}'.format(chr(ord(i) if not islower(i) else ord(i) - 32)), end='')
+    print('')
