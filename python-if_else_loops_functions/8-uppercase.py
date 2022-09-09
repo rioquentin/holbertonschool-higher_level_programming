565543b8f8d3f4b7cd84ca130d54715b99576c1d
Quentin Rio <quentin.rio.pro@gmail.com>
2022-09-09 15:18:28 +0200

    Corrected

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,5 @@ islower = __import__('7-islower').islower
 
 def uppercase(str):
     for i in str:
-        print('{}'.format(i if not islower(i) else chr(ord(i) - 32),
-              end=''))
+        print('{}'.format(i if not islower(i) else chr(ord(i) - 32)),end='')
     print('')
-
-uppercase("Holberton School")
