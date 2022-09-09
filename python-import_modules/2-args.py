8f785b65e53c6823f0efe0aa6102ce05e935b62f
Quentin Rio <quentin.rio.pro@gmail.com>
2022-09-09 15:18:28 +0200

    PEP8

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     if (len(sys.argv) - 1) == 0:
         print("0 arguments.")
     else:
-        print('{} {}:'.format(len(sys.argv) - 1,\
-            "argument" if len(sys.argv) - 1 == 1 else "arguments"))
+        if len(sys.argv) - 1 == 1:
+            print('{} {}:'.format(len(sys.arg) - 1, "argument"))
+        else:
+            print('{} {}:'.format(len(sys.arg) - 1, "arguments"))
     for i in range(1, len(sys.argv)):
         print('{}: {}'.format(i, sys.argv[i]))
