79fcb6b368797db7f07e6f7a15c07b0a6a9cb60e
quentin <quentin.rio.pro@gmail.com>
2022-09-12 06:15:08 -0700

    task 4 replace a value whithout otuching to the original list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list:
-        new = my_list.copy()
-        new[idx] = element
-        return new
+        if idx < 0 or idx > len(my_list) - 1:
+            return None
+        else:
+            new = my_list.copy()
+            new[idx] = element
+            return new
