3fc3cbfbb8f940babe9f7f2e09cbad7388de50db
quentin <quentin.rio.pro@gmail.com>
2022-09-12 02:02:23 -0700

    Task 0 print list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    print('\n'.join('{:d}'.format(i) for i in my_list))
+    if my_list:
+        print('\n'.join('{:d}'.format(i) for i in my_list))
