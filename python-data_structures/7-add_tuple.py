f6cc199dbed01586e46c3ef0ad1c59bb82921b3d
quentin <quentin.rio.pro@gmail.com>
2022-09-13 04:17:42 -0700

    Task 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def tuple_formater(a=()):
+    print(len(a))
+    if len(a) < 2:
+        new = a + (0, 0)
+        return new
+    elif len(a) > 2:
+        new = a[0:2]
+        return new
+    else:
+        return a
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_formater(tuple_a)
+    b = tuple_formater(tuple_b)
+    return(tuple(p+q for p, q in zip(a, b)))
