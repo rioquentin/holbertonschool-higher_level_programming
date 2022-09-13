2c37912e7f458dfc2962f3e0e03c589fce2ecabb
quentin <quentin.rio.pro@gmail.com>
2022-09-13 11:53:42 -0700

    Task 10 check if multiple of two

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        result = []
+        for i in my_list:
+            if i % 2 == 0:
+                result.append(true)
+            else:
+                result.append(false)
+        return result
