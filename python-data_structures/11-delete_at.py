cbb03eb077aa44596f81e2c91ce845f9708c6c06
quentin <quentin.rio.pro@gmail.com>
2022-09-13 12:03:53 -0700

    task 11 delete a idx

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    my_list.remove(my_list[idx])
-    return my_list
+    if my_list and idx <= len(my_list) - 1:
+        my_list.remove(my_list[idx])
+        return my_list
