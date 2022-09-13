fd586cd520ff4d0b9d01bfb9b49a4f5dbcca267d
quentin <quentin.rio.pro@gmail.com>
2022-09-13 01:28:07 -0700

    Task 6 print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         print('\n'.join(' '.join('{:d}'.format(i) 
-                                    for i in x) for x in matrix))
+                                 for i in x) for x in matrix))
