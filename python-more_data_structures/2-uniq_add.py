817a2e98675bd07a33fc5d3072f1b666a724becf
quentin <quentin.rio.pro@gmail.com>
2022-09-14 03:15:22 -0700

    task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    result = 0
     if my_list:
         ban = []
-        result = 0
         for i in my_list:
             if i not in ban:
                 ban.append(i)
         for x in ban:
             result += x
         return result
+    return result
