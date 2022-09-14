df5abaa2a1892a9a9f4759367322bc25e462b53e
quentin <quentin.rio.pro@gmail.com>
2022-09-14 01:00:53 -0700

    Task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
-        new = list(map(list, matrix)
+        new = list(map(list, matrix))
         for x in new:
             for i in range(len(x)):
                 x[i] = x[i] ** 2
