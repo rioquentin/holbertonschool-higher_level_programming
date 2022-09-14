28a473c576d5b9c4cf948e6c52ea7cc377a49832
quentin <quentin.rio.pro@gmail.com>
2022-09-14 11:30:52 -0700

    added first decimal

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a // b
+        result = format(a // b, '.1f')
         print("Inside result:", end='')
     except:
         result = None
