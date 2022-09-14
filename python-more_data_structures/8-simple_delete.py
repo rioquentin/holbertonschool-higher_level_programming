29b00ffc3349182ab84ec2add506c568908c1691
quentin <quentin.rio.pro@gmail.com>
2022-09-14 06:05:40 -0700

    Task 8 pop a key

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key) == None:
+    if a_dictionary.get(key) is None:
         return a_dictionary
     else:
         a_dictionary.pop(key)
