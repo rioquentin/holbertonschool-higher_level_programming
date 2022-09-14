0408eeb63f2e8ce515e21ddb4d7431a5ebb24a2d
quentin <quentin.rio.pro@gmail.com>
2022-09-14 14:25:41 -0700

    task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         for i in range(0, list_length):
             try :
                 result = format(my_list_1[i] // my_list_2[i], ".1f")
-                nl.append(result)
+                nl.append(float(result))
             except ZeroDivisionError:
                 print("division by 0")
                 nl.append(0)
