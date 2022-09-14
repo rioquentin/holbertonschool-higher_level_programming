d1799553f6edfcec3771db8460d6edd6bd68f69a
quentin <quentin.rio.pro@gmail.com>
2022-09-14 00:59:51 -0700

    Task 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix:
+        new = list(map(list, matrix)
+        for x in new:
+            for i in range(len(x)):
+                x[i] = x[i] ** 2
+        return new
+    else:
+        return matrix
