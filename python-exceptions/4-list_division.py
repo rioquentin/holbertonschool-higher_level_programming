9c3efd5d32009a62c55f055e7e13320eef861627
quentin <quentin.rio.pro@gmail.com>
2022-09-14 12:53:00 -0700

    Task 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    try:
+        nl = []
+        for i in range(0, list_length):
+            try :
+                result = format(my_list_1[i] // my_list_2[i], ".1f")
+                nl.append(result)
+            except ZeroDivisionError:
+                print("division by 0")
+                nl.append(0)
+            except TypeError:
+                print("wrong type")
+                nl.append(0)
+            except IndexError:
+                print("out of range")
+                nl.append(0)
+    finally:
+        return nl
