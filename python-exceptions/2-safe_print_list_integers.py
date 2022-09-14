121467380e7b7ce08aeeca4e97540c5dcac457f6
quentin <quentin.rio.pro@gmail.com>
2022-09-14 11:05:21 -0700

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(0, x):
             if isinstance(my_list[i], int) is True:
-                print("{}".format(my_list[i]), end='')
+                print("{:d}".format(my_list[i]), end='')
                 z += 1
         print("")
     except (ValueError, TypeError):
