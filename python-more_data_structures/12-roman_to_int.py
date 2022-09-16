3f05ae579788ddfea2b450adc08df36a2c3b4193
quentin <quentin.rio.pro@gmail.com>
2022-09-16 13:48:54 -0700

    roman to int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if isinstance(roman_string, str) is False or roman_string == None:
+        return 0
+    number = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    prev = 0
+    for i in roman_string:
+        for x in number:
+            if i == x:
+                actual = number.get(x)
+                if result != 0:
+                    if actual > prev:
+                        result = actual - prev
+                    else:
+                        result += number.get(x)
+                    prev = number.get(x)
+                else:
+                    result = number.get(x)
+                    prev = number.get(x)
+    return result
