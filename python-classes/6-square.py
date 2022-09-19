b892d7a5300168390afe049502b8d71625538b6a
quentin <quentin.rio.pro@gmail.com>
2022-09-19 07:38:30 -0700

    test

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -47,9 +47,11 @@ class Square(object):
     def my_print(self):
         ''' func to print the square'''
         if self.__size != 0:
-            for i in range(0, self.__size):
-                for z in range(0, self.__position[0]):
+            for b in range(0, self.__position[0]:
                     print("")
+            for i in range(0, self.__size):
+                for z in range(0, self.__position[1]):
+                    print("", end="")
                 for x in range(0, self.__size):
                     print("{}".format("#"), end="")
                 print("")
