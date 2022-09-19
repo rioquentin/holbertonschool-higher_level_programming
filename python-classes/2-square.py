c65f48a27521e856b2690ba6bd30f398d10ccb8e
quentin <quentin.rio.pro@gmail.com>
2022-09-19 02:48:55 -0700

    Task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
         if isdigit(size) is False:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0"
+            raise ValueError("size must be >= 0")
 
         self.__size = size
 
