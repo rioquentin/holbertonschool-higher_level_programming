7dd2eee0ae140679533d33273e7de89e2c3da8e3
quentin <quentin.rio.pro@gmail.com>
2022-09-19 03:44:47 -0700

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' Empty Class'''
+
+
+class Square:
+    '''Empty'''
+ 
+    def __init(self, size=0):
+        '''defining size''
+        self.__size = size
+
+    def size(self, value):
+        '''seting size'''
+
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+
+    def area(self):
+        '''Define size*size'''
+
+        return self.__size * self.__size
