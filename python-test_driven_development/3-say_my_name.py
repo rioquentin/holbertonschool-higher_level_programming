06340aef721f49b64f34c8d522a08ef416659f0f
quentin <quentin.rio.pro@gmail.com>
2022-09-20 13:07:44 -0700

    test

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,8 +6,6 @@
 def say_my_name(first_name, last_name=""):
     '''func to print'''
 
-    if first_name == None and last_name == None:
-        raise TypeError("missing two arguments")
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
