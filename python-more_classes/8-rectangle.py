43fe0f0145aaf1d0a9db50ba76fc4d2b0840a4d6
quentin <quentin.rio.pro@gmail.com>
2022-09-21 14:56:28 -0700

    Task 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -72,7 +72,7 @@ class Rectangle:
         '''return del message'''
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         '''static method'''
