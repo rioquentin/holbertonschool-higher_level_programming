58c012364eb5f5079860ddab7aa63dd5c2324696
quentin <quentin.rio.pro@gmail.com>
2022-09-21 12:04:01 -0700

    Task 1 V.0

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+''' Empty Class'''
+
+
+class Rectangle:
+    '''Property of class rectangle'''
+
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        '''getter'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''setter'''
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+
+    @property
+    def height(self):
+        '''getter'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''setter'''
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = value
+
+
