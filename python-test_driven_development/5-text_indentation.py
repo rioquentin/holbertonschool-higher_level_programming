4a10cdb0fe284e7007f7628981d8d9694c37a95e
quentin <quentin.rio.pro@gmail.com>
2022-09-21 06:04:21 -0700

    test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,6 +3,8 @@
 
 
 def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError("text must be a string")
     l = text.split()
     h = ""
     for i in l:
@@ -13,7 +15,7 @@ def text_indentation(text):
     for x in range(0, len(h)):
         if h[x] in ["?", ".", ":"]:
             print(h[x] + "\n")
-        elif h[x - 1] in ["?", ":", "."]:
+        elif h[x - 1] in ["?", ":", "."] and h[x] == " ":
             pass
         else:
             print(h[x], end="")
