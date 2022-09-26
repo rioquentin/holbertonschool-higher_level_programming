d9e56654ab9d93bcc2da005f5a8354bd6b223204
quentin <quentin.rio.pro@gmail.com>
2022-09-26 07:25:16 -0700

    test

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,9 +5,9 @@
 def is_same_class(obj, a_class):
     ''' check if obj is instance of a-class'''
 
-    #if type(obj) is not in [int, float, object]:
-    #    return False
-    if isinstance(obj, a_class) == True:
+    if type(obj) is not in [int, float, object]:
+        return False
+    elif isinstance(obj, a_class) == True:
         return True
     else:
         return False
