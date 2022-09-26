8655f0869a88475cc671b4952f3556537ba8eee2
quentin <quentin.rio.pro@gmail.com>
2022-09-26 07:46:35 -0700

    test

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-'''
-Test
-'''
+'''i Module'''
 
 
 def is_same_class(obj, a_class):
-    '''
-    check if obj is instance of a-class
-    '''
+    '''check if obj is instance of a-class'''
 
     if type(obj) is in a_class:
         return True
