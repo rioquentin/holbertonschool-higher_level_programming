a7ce867427e3523f4acab0cfa80334ff7a76eb64
quentin <quentin.rio.pro@gmail.com>
2022-09-27 06:04:42 -0700

    TAsk 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,8 +10,8 @@ class Square(Rectangle):
 
     def __init__(self, size):
         '''constructor for the square'''
-        if size >= 0:
-            self.__size = size
+        self.integer_validator("size", size)
+        self.__size = size
 
     def area(self):
         return self.__size * self.__size
