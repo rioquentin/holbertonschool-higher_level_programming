e2e946dfe1bba3a97d79bd756845d6da445d09b9
quentin <quentin.rio.pro@gmail.com>
2022-09-27 13:38:20 -0700

    task 0

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     '''Func'''
 
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, w+, encoding="utf-8") as f:
         return f.write(text)
