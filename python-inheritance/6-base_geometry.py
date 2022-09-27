6a50fadbee9356bc680a5b2855130c511585cc2c
quentin <quentin.rio.pro@gmail.com>
2022-09-27 01:42:40 -0700

    TAsk 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module'''
+
+
+class BaseGeometry:
+    '''Class'''
+
+    def area(self):
+        '''Method to get area'''
+
+        raise Exception("area() is not implemented")
