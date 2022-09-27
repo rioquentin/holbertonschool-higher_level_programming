de395e95dd7a0f39400b34e4178bb9e1a45fb18c
quentin <quentin.rio.pro@gmail.com>
2022-09-27 05:21:11 -0700

    Task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         '''Constructor'''
 
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
