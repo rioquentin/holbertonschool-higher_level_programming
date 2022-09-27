dfdc1f4ab98599af51c219c5247941df0f1ae30e
quentin <quentin.rio.pro@gmail.com>
2022-09-27 05:55:42 -0700

    task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Module'''
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Class'''
+
+    def __init__(self, width, height):
+        '''Constructor'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        '''print str'''
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+    
+    def area(self):
+        '''area'''
+        return self.__width * self.__height
