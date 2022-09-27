b08cef44c71b2979e8867005831ac67160dec614
quentin <quentin.rio.pro@gmail.com>
2022-09-27 14:04:25 -0700

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def append_write(filename="", text=""):
     '''append'''
 
-    with open(filename,'a' ,encoding="utf-8") as f:
+    with open(filename,'a+' ,encoding="utf-8") as f:
         return f.write(test)
