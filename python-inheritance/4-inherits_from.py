e8c3968ec05c71b59a00281d1280e679fa7d1ac3
quentin <quentin.rio.pro@gmail.com>
2022-09-27 01:37:00 -0700

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     '''Func'''
 
-    if isinstance(obj, a_class) is True:
+    if type(obj) is a_class:
         return False
     if issubclass(type(obj), a_class) is True:
         return True
