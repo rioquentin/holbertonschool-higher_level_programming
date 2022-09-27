7a9a7fe6c8c307047a1a2d96a2cde2f0c0d9557b
quentin <quentin.rio.pro@gmail.com>
2022-09-27 05:17:04 -0700

    Task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-
 '''Module'''
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class BaseGeometry:
+    '''Class'''
+
+    def area(self):
+        '''Method to get area'''
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''Check Value'''
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     '''Class'''
@@ -10,7 +25,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         '''Constructor'''
 
-        super().integer_validator(self, "width", width)
-        super().integer_validator(self, "height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
