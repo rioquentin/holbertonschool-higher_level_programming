43bb4b6cbb7fba3abec0dcd384a2354d467c6514
quentin <quentin.rio.pro@gmail.com>
2022-09-28 13:16:23 -0700

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 '''Module'''
-import json
+
+
 import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-def main(args):
-    '''func'''
-
-    f = load_from_json_file("add_item.json")
-    for i in range(1, len(sys.argv)):
-        f.append(sys.argv[i])
-    save_to_json_file(f, "add_item.json")
-
-main(sys.argv[1:])
+f = load_from_json_file("add_item.json")
+for i in range(1, len(sys.argv)):
+    f.append(sys.argv[i])
+save_to_json_file(f, "add_item.json")
