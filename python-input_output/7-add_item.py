6fc786546b010df33a4082563b225c5952fdd6e5
quentin <quentin.rio.pro@gmail.com>
2022-09-29 01:27:37 -0700

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,11 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+try:
+    f = load_from_json_file("add_item.json")
 
-f = load_from_json_file("add_item.json")
-for i in range(1, len(sys.argv)):
-    f.append(sys.argv[i])
+    for i in range(1, len(sys.argv)):
+        f.append(sys.argv[i])
+except Exception:
+    f = []
 save_to_json_file(f, "add_item.json")
