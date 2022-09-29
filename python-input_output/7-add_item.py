36bfbbde02ff15de8727a80c5194746ae8629ca0
quentin <quentin.rio.pro@gmail.com>
2022-09-29 01:28:44 -0700

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,8 +9,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 try:
     f = load_from_json_file("add_item.json")
 
-    for i in range(1, len(sys.argv)):
-        f.append(sys.argv[i])
 except Exception:
     f = []
+for i in range(1, len(sys.argv)):
+    f.append(sys.argv[i])
 save_to_json_file(f, "add_item.json")
