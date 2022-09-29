50e7776d43dfdadac3bde6c164fb646798210ecb
quentin <quentin.rio.pro@gmail.com>
2022-09-29 02:15:53 -0700

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''MOdule'''
+
+
+class Student:
+    '''Class'''
+
+    def __init__(self, first_name, last_name, age):
+        '''constructor'''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''to json'''
+        if attrs is not None:
+            return  for i in
+        else:
+            return self.__dict__[i] for i in attrs
