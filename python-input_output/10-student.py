caafe79801e1e384473403eeede7d9e8bdf7dd29
quentin <quentin.rio.pro@gmail.com>
2022-09-29 02:17:11 -0700

    task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -14,7 +14,7 @@ class Student:
 
     def to_json(self, attrs=None):
         '''to json'''
-        if attrs is not None:
-            return  for i in
+        if attrs == None:
+            return self.__dict__
         else:
             return self.__dict__[i] for i in attrs
