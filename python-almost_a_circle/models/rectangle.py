8ef475760a2a96c26e597f2f407bd9e84486525b
quentin <quentin.rio.pro@gmail.com>
2022-10-03 08:10:21 -0700

    Task 9 - added update with kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,10 +95,9 @@ class Rectangle(Base):
         return("[Rectangle] ({}) {}/{} - {}/{}".format
                (self.id, self.x, self.y, self.width, self.height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         '''method to update the rectangle'''
-
-        if args[0]:
+        if args:
             self.id = args[0]
             if len(args) >= 2:
                 self.width = args[1]
@@ -108,3 +107,6 @@ class Rectangle(Base):
                         self.x = args[3]
                         if len(args) >= 5:
                             self.y = args[4]
+        else:
+            for i, x in kwargs.items():
+                setattr(self, i, x)
