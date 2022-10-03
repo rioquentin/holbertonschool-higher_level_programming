5544558d0a9d35eac64cbc68ca4f202abc982569
quentin <quentin.rio.pro@gmail.com>
2022-10-03 01:38:11 -0700

    trying to fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,11 +6,12 @@ class Base:
     '''Main class with basics methods'''
 
     __nb_objects = 0
+
     def __init__(self, id=None):
         '''Constructor for base attributes'''
 
-        if id is not None:
-            self.id = id
-        else:
+        if id is None:
             __nb_objects += 1
             self.id = __nb_objects
+        else:
+            self.id = id
