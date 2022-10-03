fd679420b1deca552df823d7eea3cabefa7300d5
quentin <quentin.rio.pro@gmail.com>
2022-10-03 02:47:28 -0700

    Pep8 fixed

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,31 +16,42 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    #getter and setter for width parameter
 
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         self.__width = value
+
+    #getter and setter for height parameter
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         self.__height = value
+
+    #getter and setter for x parameter
+
     @property
     def x(self):
         return self.__x
+
     @x.setter
     def x(self, value):
         self.__x = value
+
+    #getter and setter for y parameter
+
     @property
     def y(self):
         return self.__y
-    @width.setter
+
+    @y.setter
     def y(self, value):
         self.__y = value
-
-
-
