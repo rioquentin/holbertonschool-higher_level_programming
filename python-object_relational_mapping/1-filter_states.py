7c47d5e3b4bf5743dcf5308c015e1ff4d93ce6f4
root <you@example.com>
2022-11-12 13:26:05 -0800

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""ORM SCRIPT"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'"
+                "ORDER BY id ASC;""")
+    result = c.fetchall()
+
+    for i in result:
+        print(i)
