5a1acd34a33f6456f74f7348c7dc194c30ac3c21
root <you@example.com>
2022-11-12 14:50:15 -0800

    task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """SQLALCHEMY"""
 
-from sqlalchemy import create_engine
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     session = sessionmaker(bind=engine)()
     result = session.query(State).all()
-    for i in result:
-        print(i)
+    for row in result:
+        print(row)
     session.close()
