f41d55004d7c1e869133dbe0c25be2515514025e
root <you@example.com>
2022-11-12 13:59:19 -0800

    task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""ORM SCRIPT"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT cities.name FROM cities
+            LEFT JOIN states ON states.id = cities.state_id
+            WHERE states.name = %s ORDER BY cities.id ASC""", (sys.argv[4], ))
+    result = c.fetchall()
+
+    last = len(result)
+    for i in range(0, len(result) - 1):
+        print(result[i] + ", " end="")
+    print(result[last])
+        
