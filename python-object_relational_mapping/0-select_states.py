4a088e2547c872100ab6bea9ea65a4b2621fa734
root <you@example.com>
2022-11-12 13:18:39 -0800

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""ORM SCRIPT"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states;""")
+    result = c.fetchall()
+
+    for i in result:
+        print(i)
