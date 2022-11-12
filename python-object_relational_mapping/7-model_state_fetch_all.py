ed9dc8f56cd060c240d906ae12cdb6f241da45c3
root <you@example.com>
2022-11-12 14:52:21 -0800

    task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""SQLALCHEMY"""
+"""list all State objects"""
 
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
