587858e44bd044fa6aabbe77021d5d2be876c378
root <you@example.com>
2022-12-08 02:21:58 -0800

    Task 3 - Print the content of argv

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,10 +2,9 @@
 const process = require('process');
 const argv = process.argv;
 let i = 2;
-if (argv.length === 2) {
+if (argv[2] === undefined) {
   console.log('No argument');
-} else if (argv.length === 3) {
-  console.log(argv[i]);
+
 } else {
   while (1) {
     if (argv[i] != undefined){
