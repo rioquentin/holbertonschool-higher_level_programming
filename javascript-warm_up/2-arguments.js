cb08aebfeca96d4dc2bd0ebab8113a4e052bb7ff
root <you@example.com>
2022-12-08 02:05:17 -0800

    Task 2 - Check if there is argmuent(s) in the command line

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
 const argv = process.argv;
-if (argv.length == 2){
+if (argv.length === 2) {
   console.log('No argument');
-} else if (argv.length == 3) {
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
