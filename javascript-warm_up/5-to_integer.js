830fad8527a6e27987e59e7c7f28c6e824c859e3
root <you@example.com>
2022-12-08 02:41:29 -0800

    Task 5 - Check if the argument is a number

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 const argv = process.argv;
-if (isNaN(argv[2]) != true) {
+if (isNaN(argv[2]) !== true) {
   console.log('My number:', argv[2]);
 } else {
   console.log('Not a number');
