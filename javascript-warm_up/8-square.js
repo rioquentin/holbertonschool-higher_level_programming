8ea4661ad73992a6b8070fad8e749958a832dc2e
root <you@example.com>
2022-12-09 05:24:56 -0800

    Fixed if x is not a number

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,12 +3,18 @@ const process = require('process');
 const argv = process.argv;
 let i = 0;
 let x = 0;
-while (i < argv[2]) {
-  x = 0;
-  while (x < argv[2]) {
-    process.stdout.write('X');
-    x += 1;
+
+if (isNaN(argv[2])) {
+  console.log('Missing size');
+  process.exit(0);
+} else {
+  while (i < argv[2]) {
+    x = 0;
+    while (x < argv[2]) {
+      process.stdout.write('X');
+      x += 1;
+    }
+    console.log('');
+    i += 1;
   }
-  console.log('');
-  i += 1;
 }
