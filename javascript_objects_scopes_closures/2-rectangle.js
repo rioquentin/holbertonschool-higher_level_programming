290d56a6cef7f19e27e53c100b0fbc5997f1a65c
root <you@example.com>
2022-12-11 12:41:31 -0800

    Fixed wrong declaration

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (h <= 0 || w <= 0 || h === undefined || w === undefined) {
-      this.width = undefined;
-      this.height = undefined;
+      ;
     } else {
       this.width = w;
       this.height = h;
