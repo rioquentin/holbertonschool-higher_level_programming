cf90f1ec82b7e7a7fbc069a36d38d8239538dcdc
root <you@example.com>
2022-12-11 12:37:58 -0800

    Task 13 - read and write file

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -8,5 +8,5 @@ const pathFile1 = path.resolve(path.dirname(__filename), './' + argv[2]);
 const pathFile2 = path.resolve(path.dirname(__filename), './' + argv[3]);
 const pathFile3 = path.resolve(path.dirname(__filename), './' + argv[4]);
 
-const input = fs.readFileSync(pathFile1, 'utf8') + '\n' + fs.readFileSync(pathFile2, 'utf8');
+const input = fs.readFileSync(pathFile1, 'utf8') + fs.readFileSync(pathFile2, 'utf8');
 fs.writeFileSync(pathFile3, input);
