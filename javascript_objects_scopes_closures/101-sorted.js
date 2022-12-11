b55311906689f799cbe1e365009a4660f9113940
root <you@example.com>
2022-12-11 11:37:58 -0800

    task 12 - sort a dict by key

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -2,23 +2,13 @@
 const importDict = require('./101-data');
 
 const _dict = importDict.dict;
+const newDict = {};
 
-const a = [];
-const b = [];
-const c = [];
-
-for (const key in _dict) {
-  if (_dict[key] === 1) {
-    a.push(key);
-  } else if (_dict[key] === 2) {
-    b.push(key);
-  } else if (_dict[key] === 3) {
-    c.push(key);
+for (const userId in _dict) {
+  const occurrences = _dict[userId];
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
   }
+  newDict[occurrences].push(userId);
 }
-const newDict = {
-  1: a,
-  2: b,
-  3: c
-};
 console.log(newDict);
