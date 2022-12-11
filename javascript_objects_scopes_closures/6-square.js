deb7a145d235bcf008b1fad6ef9f9c97e4e26b59
root <you@example.com>
2022-12-11 06:20:57 -0800

    Task 6 - Fixed semistandard error

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const Square_1 = require('./5-square');
+const Square1 = require('./5-square');
 
-class Square extends Square_1 {
-  constructor(s) {
-    super(s);
-  }
-  charPrint(c) {
+class Square extends Square1 {
+  charPrint (c) {
     super.charPrint(c);
   }
 }
