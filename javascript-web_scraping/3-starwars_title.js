3461b02188fde37712c0ecd2da0401e6c24e3a68
root <you@example.com>
2022-12-11 21:24:48 -0800

    Task 3 - Get data from SWAPI

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
 const request = require('request');
-let id = process.argv[2];
+const id = process.argv[2];
 
-const idFilm = id + "/"; // the id of the film you want to retrieve
+const idFilm = id + '/'; // the id of the film you want to retrieve
 request(`https://swapi.dev/api/films/${idFilm}`, (error, response, body) => {
   if (error) {
     console.error(error);
