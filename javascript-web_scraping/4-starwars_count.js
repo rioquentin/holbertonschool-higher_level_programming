f26d87bcee30e87e2efcca8dbe5ef37fa8a20210
root <you@example.com>
2022-12-12 10:23:07 -0800

    task 4 - parse ata

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+const CHARACTER_ID = 18;
+function getData (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+}
+function characterInFilm (film, characterId) {
+  return film.characters.some((characterUrl) => {
+    const characterIdRegex = new RegExp(`/${characterId}/$`);
+    return characterUrl.match(characterIdRegex);
+  });
+}
+async function main () {
+  const filmsData = await getData(API_URL);
+  const films = filmsData.results;
+  let count = 0;
+  films.forEach((film) => {
+    if (characterInFilm(film, CHARACTER_ID)) {
+      count += 1;
+    }
+  });
+  console.log(count);
+}
+main();
