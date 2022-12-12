3ce1646b37952e6fa22d2ea2b6fdd8267ef2ad30
root <you@example.com>
2022-12-11 20:03:33 -0800

    Task 0 - read from another file and print the content

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,9 +3,8 @@ const process = require('process');
 const argv = process.argv;
 const fs = require('fs');
 const path = require('path');
-
-if (argv[2]) {
-  const pathFile = path.resolve(path.dirname(__filename), './' + argv[2]);
+const pathFile = path.resolve(path.dirname(__filename), './' + argv[2]);
+if (fs.existsSync(pathFile)) {
   const input = fs.readFileSync(pathFile, 'utf8');
   console.log(input);
 }
