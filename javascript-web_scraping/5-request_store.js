4bba25bc27bcaf114920c78bd74938dad164a856
root <you@example.com>
2022-12-12 10:38:08 -0800

    Task 5 - scrap data and write it in a file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,8 +6,7 @@ request(process.argv[2], (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const data = JSON.parse(body);
-    fs.writeFile(data, process.argv[3], 'utf8', (error) => {
+    fs.writeFile(body, process.argv[3], 'utf8', (error) => {
       if (error) {
         console.error(error);
       }
