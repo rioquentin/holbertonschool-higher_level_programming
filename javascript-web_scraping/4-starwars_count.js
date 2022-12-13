c022200cdf5faaee01c7cce81001473786f23e6f
root <you@example.com>
2022-12-13 05:09:54 -0800

    Task 4 - fix semistandard

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+const API_URL = process.argv[2];
 const CHARACTER_ID = 18;
 function getData (url) {
   return new Promise((resolve, reject) => {
