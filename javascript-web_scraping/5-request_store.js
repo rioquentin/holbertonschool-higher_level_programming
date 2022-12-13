7703f6da9fd9ce7a853c679fdd8875bce6efd082
root <you@example.com>
2022-12-13 05:15:54 -0800

    TAsk 5 -fixed wrong order for the write

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-
 request(process.argv[2], (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    fs.writeFile(body, process.argv[3], 'utf8', (error) => {
+    fs.writeFile(process.argv[3], body, 'utf8', (error) => {
       if (error) {
         console.error(error);
       }
